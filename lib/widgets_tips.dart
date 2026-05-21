e77bf3fb811539206b931f2922aba6fc3c9fe80e
Erlanbek <kanybekov668@gmail.com>
2026-05-21 23:52:55 +0600

    one

--- lib/widgets_tips.dart
+++ lib/widgets_tips.dart
@@ -5,6 +5,11 @@ class MyWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+         Expanded(child: Text("data"))
+      ],
+    );
   }
 }
