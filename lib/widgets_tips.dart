59d331849f70d6b008dd649d05b69770d73195ad
Erlanbek <kanybekov668@gmail.com>
2026-05-22 21:41:04 +0600

    i need only one

--- lib/widgets_tips.dart
+++ lib/widgets_tips.dart
@@ -5,6 +5,19 @@ class MyWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Expanded(child: Text("data")),
+        Expanded(child: Text("data")),
+        Flexible(child: Text("I don't know about your featA")),
+        Flexible(child: Text("I don't know about your commits on featA")),
+        Flexible(child: TextButton(onPressed: () {}, child: Text("feat click"))),
+        Text("develop saved trying to clear history with reset"),
+        Text("1"),
+        Text("2"),
+        Text("3")
+      ],
+    );
   }
 }
